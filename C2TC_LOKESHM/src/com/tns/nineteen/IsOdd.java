7bbab62f7c1453e4096f33d3060944797c550afe
lokesh <lokesh@DESKTOP-TCSTCIA>
2025-01-23 10:53:24 +0530

    initial commit

---
+++ C2TC_LOKESHM/src/com/tns/nineteen/IsOdd.java
@@ -0,0 +1,7 @@
+//Program to demonstrate Functional Interface
+package com.tns.nineteen;
+
+@FunctionalInterface
+interface IsOdd{
+	public boolean checkOdd(int a);
+}
---
+++ C2TC_LOKESHM/src/com/tns/nineteen/Message.java
@@ -0,0 +1,7 @@
+package com.tns.nineteen;
+
+@FunctionalInterface
+public interface Message{  
+    public void greet(String name);    
+ } 
+
